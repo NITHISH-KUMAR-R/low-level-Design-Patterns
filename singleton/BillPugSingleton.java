d8aa43a6a6ccadabe5efa40a2d9abda8e488c262
Nithish Kumar <nithishlearning45@gmail.com>
2024-11-30 18:08:22 +0530

    BillPugSingleton

---
+++ singleton/BillPugSingleton.java
@@ -0,0 +1,13 @@
+package com.designpattern.lld.singleton;
+
+public class BillPugSingleton {
+    private BillPugSingleton(){
+        System.out.println("hello for first time");
+    }
+    public static class Singleton{
+        private static final BillPugSingleton INSTANCE = new BillPugSingleton();
+        public static BillPugSingleton getInstance(){
+            return INSTANCE;
+        }
+    }
+}
