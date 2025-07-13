029a2de667d7c1b216aeb35ee3cce6c22f36b0dd
Nithish Kumar <nithishlearning45@gmail.com>
2025-07-13 22:32:00 +0530

    Final Parking lot Pricing Strategy

---
+++ parking/PricingStrategyFactory.java
@@ -0,0 +1,26 @@
+package com.designpattern.lld.parking;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class PricingStrategyFactory {
+    public static  PricingStrategy getPricingStragegy(String contextType){
+        DayOfWeek today = LocalDate.now().getDayOfWeek();
+
+        switch (contextType){
+            case  "concert":
+                return new ConcertPricingStrategy();
+            case "weekday":
+                return new WeekDayPricing();
+            case "weekend":
+                if(today == DayOfWeek.SATURDAY || today == DayOfWeek.SUNDAY){
+                    return new WeekEndPricingStrategy();
+                }
+                else{
+                    return new WeekDayPricing();
+                }
+            default:
+                throw new IllegalArgumentException("Unknown context type: need business team to decide " + contextType);
+        }
+    }
+}
--- parking/PricingStrategyPatternMainClass.java
+++ parking/PricingStrategyPatternMainClass.java
@@ -1,14 +1,22 @@
 package com.designpattern.lld.parking;
 
+import java.util.Scanner;
+
 public class PricingStrategyPatternMainClass {
 
     public static void main(String[] args) {
-        PriceCalculator priceCalculator = new PriceCalculator(new ConcertPricingStrategy());
-        System.out.println(priceCalculator.getPrice(2));
 
-        priceCalculator.setPricingStrategy(new WeekEndPricingStrategy());
-        System.out.println(priceCalculator.getPrice(2));
-        priceCalculator.setPricingStrategy(new WeekDayPricing());
-        System.out.println(priceCalculator.getPrice(2));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Pricing Context (concert / weekday / weekend )");
+        String contextType = sc.nextLine().trim();
+
+
+            PricingStrategy pricingStrategy = PricingStrategyFactory.getPricingStragegy(contextType);
+            System.out.println("Enter No of hours Parked");
+            int hour = sc.nextInt();
+            double price  = pricingStrategy.calculatePrice(hour);
+            System.out.println("Total Parking fee "+price);
+
+
     }
 }
